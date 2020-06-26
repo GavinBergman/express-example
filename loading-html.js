02e067b5667000953198ad1a0c4c2294cd8cce98
GavinBergman <49213409+GavinBergman@users.noreply.github.com>
2020-06-26 01:21:15 -0600

    Update loading-html.js

--- loading-html.js
+++ loading-html.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
 app.listen(8080, () => console.log('listening at 8080'));
-app.use( express.static('public'));
+app.use( express.static('index.html'));
