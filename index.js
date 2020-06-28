7145706ef397e2d6c323747f23f9f1d3e9ff7d77
GavinBergman <49213409+GavinBergman@users.noreply.github.com>
2020-06-28 11:10:29 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.listen(8080, () => console.log('listening at 8080'));
 app.use( express.static('index.html'));
+
+app.listen(8080, () => console.log('listening at 8080'));
+
