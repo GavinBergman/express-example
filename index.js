f2adbce380c56b216c148d90a07c95abae12841a
GavinBergman <49213409+GavinBergman@users.noreply.github.com>
2020-07-10 16:41:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname + '/public/index.html'));
+res.sendFile(path.join(__dirname + 'index.html'));
+  // you can also say res.sendFile(path.join(__dirname + '/folder-name/index.html')); if you want to put the html in a folder
   console.log('new user has connected to the server');
 });
 
