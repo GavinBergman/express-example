e92188f38d7151d8f5c7ddd7419c2d49b13b06da
GavinBergman <49213409+GavinBergman@users.noreply.github.com>
2020-07-10 16:38:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const port = 8080;
 
-app.use( express.static('index.html'));
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname + '/public/index.html'));
+  console.log('new user has connected to the server');
+});
 
-app.listen(8080, () => console.log('listening at 8080'));
+app.listen(port, console.log('listening at 8080'));
 
