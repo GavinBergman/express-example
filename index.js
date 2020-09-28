6f856308c06c8ab8c899390b9a42c6ad152230e0
GavinBergman <49213409+GavinBergman@users.noreply.github.com>
2020-09-28 12:19:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname + 'index.html'));
-  // you can also say res.sendFile(path.join(__dirname + '/folder-name/index.html')); if you want to put the html in a folder
+res.sendFile(path.join(__dirname + '/index.html'));
   console.log('new user has connected to the server');
 });
 
-app.listen(port, console.log('listening at 8080'));
+app.listen(port, () => {
+console.log('listening to port' + port)
+});
 
